5c284044e339b71c7f44dbec032a17ebc58bfb26
Alessandro <alefrosty06@gmail.com>
2026-03-23 13:16:03 +0100

    Introduzione alle socket TCP

--- IntroduzioneUDP/client_introduzioneUDP.py
+++ IntroduzioneUDP/client_introduzioneUDP.py
@@ -2,7 +2,7 @@ import socket
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 5005
 BUFFER_SIZE = 1024
-NUM_MESSAGES = 1
+NUM_MESSAGES = 5
 
 #Creazione del socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
